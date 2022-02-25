733e63fe644a97aab28341018a662941c3ed74ee
bishalregmi105B <89974678+bishalregmi105B@users.noreply.github.com>
2022-02-25 15:28:51 +0545

    Looping Tutorial

---
+++ parts/loop.py
@@ -0,0 +1,11 @@
+#For___Loop
+#For loop is a loop which loops an statements inside loop with a given timing
+for i in range(99,100):
+    print(i)
+####While...Loop
+#####While loop is a loop which loops an statement inside it in matched conditon
+a=1
+while a<=10:
+    print(a)
+    a+=1
+print("Program Ended")
